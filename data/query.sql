e70a8d39bbb1084656d3c339f57d10ca600ada95
Deborah Ferraz Pontes <ferrazpontesdeborah@gmail.com>
2025-11-11 22:47:27 -0300

    Trabalho das Divas

--- data/query.sql
+++ data/query.sql
@@ -1,14 +1,14 @@
 --certo modelo determina marca 
-SELECT modelo, marca
-SELECT modelo
+-- se retornar 0 linhas, a dependência é VÁLIDA.
+SELECT modelo, COUNT(DISTINCT marca) AS marcas_distintas
 FROM carros
 GROUP BY modelo
-HAVING COUNT(DISTINCT marca) > 1;FROM carros
-ORDER BY modelo;
+HAVING COUNT(DISTINCT marca) > 1;
 
 
 --consulta errada ano nao depende de modelo
-SELECT ano
+-- se retornar > 0 linhas, a dependência é INVÁLIDA.
+SELECT ano, COUNT(DISTINCT modelo) AS modelos_distintos
 FROM carros
 GROUP BY ano
 HAVING COUNT(DISTINCT modelo) > 1;
